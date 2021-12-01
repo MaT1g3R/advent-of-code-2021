d58b5731b32630f5117834532d473ba060014b9c
Peijun Ma <peijun.ma@protonmail.com>
2021-12-01 14:39:23 -0500

    Remove unused line

--- day1/app/Main.hs
+++ day1/app/Main.hs
@@ -12,5 +12,4 @@ main = do
   content <- readFile "input"
   let lns = lines content
   let nums :: [Int] = fmap read lns
-  let scan = Lib.depthChanges nums
   print $ Lib.countDepthIncrease . Lib.depthChanges $ nums
